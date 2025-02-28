5dd28e4454f4ccd0c109621788106ba81f9c402b
gabrielwondracek <gabihw.2005@gmail.com>
2025-02-27 22:30:22 -0300

    Agora da para adicionar título e descrição de novas tarefas

--- src/AddTasks.jsx
+++ src/AddTasks.jsx
@@ -1,7 +1,19 @@
-function AddTasks() {
+import { useState } from "react";
+
+function AddTasks({onAddTaskSubmit}) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   return (
     <div>
-      <h3>Adicionar tarefa</h3>
+      <input type="text" placeholder="Digite a tarefa" value={title} onChange={(event) => setTitle(event.target.value)}></input>
+      <input type="text" placeholder="Digite a descrição da tarefa" value={description} onChange={(event) => setDescription(event.target.value)}></input>
+      <button onClick={() => {
+        if (!title.trim() || !description.trim()) {return alert("Favor preencher os campos vazios");}
+
+        onAddTaskSubmit(title, description)
+        setTitle("");
+        setDescription("");
+      }}>Adicionar</button>
     </div>
   );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import { useState } from "react";
 import AddTasks from "./AddTasks";
 import Tasks from "./Tasks";
+import {v4} from "uuid";
 
 function App() {
   const [tasks, setTasks] = useState([
     {
       id: 1,
       title: "ola",
+      description: "teste",
       isCompleted: false,
     },
     {
       id: 2,
       title: "oi",
+      description: "teste",
       isCompleted: false,
     },
     {
       id: 3,
       title: "tchau",
+      description: "teste",
       isCompleted: false,
     },
   ]);
@@ -38,13 +42,23 @@ function App() {
     setTasks(newTasks);
   }
 
+  function onAddTaskSubmit (title, description) {
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] ">
         <h1 className="text-3xl font-bold text-slate-100 text-center">
           Gerenciador de Tarefas
         </h1>
-        <AddTasks />
+        <AddTasks onAddTaskSubmit={onAddTaskSubmit}/>
         <Tasks
           tasks={tasks}
           onTaskClick={onTaskClick}
